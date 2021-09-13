9965eedd5147b472598ab08a597156dbaa78f00c
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2021-09-13 10:08:33 -0400

    Add files via upload

---
+++ worseCompany.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require("path");
+
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/Images'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/save', (req, res) => {
+res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
