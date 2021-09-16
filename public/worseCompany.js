31ff65a22eade11f72ad2612ed21a034a6f0fc7c
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2021-09-16 14:16:37 -0400

    The Final Version
    V 1.0 complete with an Okuyasu 404 page

---
+++ public/worseCompany.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require("path");
+
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/Images'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname+'/index.html'));
+})
+app.get('/about', (req, res) => {
+res.sendFile(path.join(__dirname+'/about.html'));
+})
+app.get('/Riley', (req, res) => {
+res.sendFile(path.join(__dirname+'/Riley.html'));
+})
+app.get('/Jenna', (req, res) => {
+res.sendFile(path.join(__dirname+'/jenna.html'));
+})
+app.get('/Dylan', (req, res) => {
+res.sendFile(path.join(__dirname+'/dylan.html'));
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname+'/404.html'));
+
+});
